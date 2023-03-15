978a5ff1ca3090e49068dfe595ba2b5fb519d540
Ramiro <ramirocoronelcardenas@gmail.com>
2023-03-15 13:17:43 -0300

    se agrego la conexion a la bd de produccion

--- src/database.js
+++ src/database.js
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
 
-const url = `mongodb://localhost:27017/cafecito-c8i`;
+//const url = `mongodb://localhost:27017/cafecito-c8i`; //BD local
+const url ="mongodb+srv://RamaCoronel:kokin1042@cluster0.mplop5p.mongodb.net/Cafe-c8i"
+//const url = `mongodb://localhost:27017/cafecito-c8i`;
 
 mongoose.connect(url);
 
